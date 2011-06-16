0d8934c872469d57c4ce68e88424ca7f67fa27d4
Kanasansoft <kanasansoft@gmail.com>
2011-06-17 00:41:52 +0900

    ブックマークレットと挿入用スクリプトタグのコードを表示するようにした。

--- src/main/resources/html/home.js
+++ src/main/resources/html/home.js
@@ -13,8 +13,19 @@ function initial(){
 		'document.body.appendChild(script);',
 		'})();'
 	];
+	var tags=[
+		'<script',
+		' type="text/javascript"',
+		' src="'+jsurl+'"',
+		' data-xiangpian-execute-url="'+wsurl+'"',
+		' data-xiangpian-execute_from="script_tag"',
+		'>',
+		'</script>'
+	];
 	var bookmarklet="javascript:"+encodeURIComponent(bookmarklets.join(""));
 	document.getElementById("bookmarklet").href=bookmarklet;
 	document.getElementById("bookmarklet_for_ios").href="?Xiangpian&-----&"+bookmarklet;
+	document.getElementById("bookmarklet_code").value=bookmarklet;
+	document.getElementById("script_tag_code").value=tags.join("");
 }
 window.addEventListener("load",initial,false);
